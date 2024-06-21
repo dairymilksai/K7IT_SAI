eb4c84e968f0fc0eeff47b5fdf31c6f3120b2a39
tripurala sai <dairymilksai2@gmail.com>
2024-06-21 19:24:20 +0530

    jack

---
+++ $HannuManager.java
@@ -0,0 +1,29 @@
+class $Hannu{
+int age; 
+double height;
+double weight;
+String name;
+String colour;
+}
+class $HannuManager{
+public static void main (String args []){
+$Hannu s = new $Hannu();
+s.age = 18;
+s.height = 5.7;
+s.weight = 30.5;
+s.name = "sai";
+s.colour = "black";
+System.out.println("SAI details");
+System.out.println("age:"+s.age );
+System.out.println("height:"+s.height);
+System.out.println("weight:"+s.weight);
+System.out.println("name:"+s.name);
+System.out.println("colour:"+s.colour); 
+}
+}
+
+
+
+
+
+
